e4e10ba3ff325a5ffe697d9f3a4e8ea6c1fbb92c
Emad Alghamdi <37039039+Eaalghamdi@users.noreply.github.com>
2022-01-11 12:57:27 +0300

    added VP list

--- slvc.py
+++ slvc.py
@@ -3,29 +3,69 @@ import pandas as pd
 import numpy as np
 
 
+data = pd.read_csv("PV_lemtaized.csv")
+VP = data.set_index('File name')
 
 
-videoURLS = ["https://www.youtube.com/watch?v=5qap5aO4i9A", "https://www.youtube.com/watch?v=5qap5aO4i9A", "https://www.youtube.com/watch?v=5qap5aO4i9A", "https://www.youtube.com/watch?v=5qap5aO4i9A", "https://www.youtube.com/watch?v=5qap5aO4i9A"]
+videoURLS = [
+    "https://www.youtube.com/watch?v=5qap5aO4i9A",
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A"
+         "https://www.youtube.com/watch?v=5qap5aO4i9A",
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A"
+         "https://www.youtube.com/watch?v=5qap5aO4i9A",
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A"
+         "https://www.youtube.com/watch?v=5qap5aO4i9A",
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A"
+         "https://www.youtube.com/watch?v=5qap5aO4i9A",
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A"
+         "https://www.youtube.com/watch?v=5qap5aO4i9A",
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A", 
+     "https://www.youtube.com/watch?v=5qap5aO4i9A"
+     ]
 
 
 def videoDisplayer(videoURLs):
-    columns = st.columns(len(videoURLS))
-    for column, videoURL in zip(columns, videoURLS):
-        column.video(videoURL)
+    for col in range(0, len(videoURLS),4):
+        columns = st.columns(3)
+        for column, videoURL in zip(columns, videoURLS):
+            column.video(videoURL)
 
 def keywordSearch(input):
     ketwords = input.split(",")
-    for k in ketwords:
-        st.write('The current movie title is', k)
+    for x in ketwords + VP["PV_list_lemmtaized"]:
+    
+        X = x.replace(", ", ",")
+        VP_list = X.split(",")
+    
+        if x in ketwords and x in VP_list:
+            st.write(x)
 
+            
 
-
+ 
 
 title = st.text_input('Search for a word in videos')
 keywordSearch(title)
 
-videoDisplayer(videoURLS)
- 
+# videoDisplayer(videoURLS)
+
 
 option = st.sidebar.selectbox(
     "Video Genre",
