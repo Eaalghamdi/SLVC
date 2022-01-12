2d1f491f7e881d49dddd67c55005d757d0c13588
Emad Alghamdi <37039039+Eaalghamdi@users.noreply.github.com>
2022-01-12 12:26:41 +0300

    add features

--- slvc.py
+++ slvc.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 
 
-data = pd.read_csv("PV_lemtaized.csv")
-VP = data.set_index('File name')
+df = pd.read_csv("PV_lemtaized.csv")
+VP = df.set_index('File name')
 
 
 videoURLS = [
@@ -51,7 +51,8 @@ def keywordSearch(input):
     keywords = input.split(",")
     videos = []
 
-    for index, row in data.iterrows():
+    # videos = df.loc[df['PV_list_lemmtaized'].isin(keywords)]
+    for index, row in df.iterrows():
     
         X = row["PV_list_lemmtaized"].replace(", ", ",")
         VP_list = X.split(",")
@@ -62,24 +63,48 @@ def keywordSearch(input):
         if result:
             videos.append(row['File name'])
   
-    st.write(videos)
             
 
-            
-
- 
+st.header("The Second Language Video Corpus")
 
 title = st.text_input('Search for a word in videos')
 keywordSearch(title)
 
-# videoDisplayer(videoURLS)
+videoDisplayer(videoURLS)
+
+#### Sidebar ####
+
+with st.sidebar.form("my_form"):
+
+    st.subheader("Filter options")
+
+    video_gnere_option = st.selectbox(
+        "Select Video Genre",
+        ("Academic Lectures", "Government advertisement")
+    )
+    if video_gnere_option  == "Academic Lectures":
+        st.write("Academic Lectures")
+    if video_gnere_option  == "Government advertisement":
+        st.write("Government advertisement")
 
 
-option = st.sidebar.selectbox(
-    "Video Genre",
-    ("Academic Lectures", "Government advertisement")
-)
-if option == "Academic Lectures":
-    st.write("Academic Lectures")
-if option == "Government advertisement":
-    st.write("Government advertisement")
+
+    values = st.slider(
+        'Select a range of difficulty values',
+        5.0, 25.0, (5.0, 20.0))
+
+    values_list =list(values)
+    Difficulty_df = df[df['Difficulty Score (5-25)'].between(values_list[0], values_list[1])]
+
+    COCA_list_options = st.multiselect(
+     'Lexical coverage',
+     ['K1', 'K1-2', 'K1-3', 'K1-4'])
+
+    slider_val = st.slider("Form slider", 0,25)
+    checkbox_val = st.checkbox("Form checkbox")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+
